3e3f58fdf5b412409672f1a75c2374cf523ac896
motwnb <98505614+MotWnb@users.noreply.github.com>
2026-07-10 12:20:47 +0800

    refactor: replace Unsafe field access

--- src/main/java/net/earthcomputer/clientcommands/util/UnsafeUtils.java
+++ src/main/java/net/earthcomputer/clientcommands/util/UnsafeUtils.java
@@ -18,34 +18,16 @@ public final class UnsafeUtils {
 
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    private static final sun.misc.@Nullable Unsafe UNSAFE = Util.make(() -> {
-        try {
-            final Field unsafeField = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
-            unsafeField.setAccessible(true);
-            return (sun.misc.Unsafe) unsafeField.get(null);
-        } catch (Exception e) {
-            LOGGER.error("Could not access theUnsafe", e);
-            return null;
-        }
-    });
-
     private static final MethodHandles.@Nullable Lookup IMPL_LOOKUP = Util.make(() -> {
         try {
-            //noinspection ConstantValue
-            if (UNSAFE == null) {
-                return null;
-            }
             final Field implLookupField = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-            return (MethodHandles.Lookup) UNSAFE.getObject(UNSAFE.staticFieldBase(implLookupField), UNSAFE.staticFieldOffset(implLookupField));
+            implLookupField.setAccessible(true);
+            return (MethodHandles.Lookup) implLookupField.get(null);
         } catch (Exception e) {
             LOGGER.error("Could not access IMPL_LOOKUP", e);
             return null;
         }
     });
-
-    public static sun.misc.@Nullable Unsafe getUnsafe() {
-        return UNSAFE;
-    }
 
     public static MethodHandles.@Nullable Lookup getImplLookup() {
         return IMPL_LOOKUP;
